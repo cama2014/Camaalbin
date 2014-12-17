7c87615f71f8c540a26f9290c6760a1b111d37b0
Albin <Albin@Albin-Dator.lan>
2014-12-17 17:45:45 +0100

    #2

---
+++ footer.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>CAMA</title>
+
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="footer.css" rel="stylesheet">
+
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        
+                <!--footer-->
+
+                <div class="row footer-row">
+                    <div class="container footer-container">
+                        <div class="row footer-row">
+
+                            <div class="col-lg-4 footer">
+                                <h2>Kontaka oss!</h2>
+                                <p>Tel: 076-422 2353</p>
+                                <p>Tel öppettider mån-fre 10:00-18:00</p>
+                                <p>kundtjanst.cama@live.se</p>
+                                <p>Butik på Kungsgatan 16</p>
+                            </div>
+
+                            <div class="col-lg-3 footer">
+                                <h2>Kille:</h2>
+                                <a href="#"><p>T-shirt</p></a>
+                                <a href="#"><p>Tröjor</p></a>
+                                <a href="#"><p>Skjortor</p></a>
+                                <a href="#"><p>Jackor</p></a>
+                                <a href="#"><p>Jeans</p></a>
+                                <a href="#"><p>Chinos</p></a>
+                                <a href="#"><p>Mössor</p></a>
+                                <a href="#"><p>Kepsar</p></a>
+                            </div>
+
+                            <div class="col-lg-3 footer">
+                                <h2>Tjej:</h2>
+                                <a href="#"><p>T-shirt</p></a>
+                                <a href="#"><p>Tröjor</p></a>
+                                <a href="#"><p>Skjortor</p></a>
+                                <a href="#"><p>Jackor</p></a>
+                                <a href="#"><p>Jeans</p></a>
+                                <a href="#"><p>Chinos</p></a>
+                                <a href="#"><p>Mössor</p></a>
+                                <a href="#"><p>Kepsar</p></a>
+                            </div>
+
+                            <div class="col-lg-2 footer">
+                                <h2>Sociala medier</h2>
+
+                                <a href ="www.facebook.com"><img src="img/facebook-icon.png"></a>
+                                <a href ="www.twitter.com"><img src="img/twitter-icon.png"></a>
+                                <a href ="www.twitter.com"><img src="img/twitter-icon.png"></a>
+                                <a href ="www.twitter.com"><img src="img/twitter-icon.png"></a>
+                            </div>   
+
+                        </div>
+                    </div> 
+                </div>
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
--- nav.php
+++ nav.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Bootstrap 101 Template</title>
+        <title>CAMA</title>
 
         <!-- Bootstrap -->
         <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -38,60 +38,115 @@
                             </button>
                             <a class="navbar-brand" href="#">CAMA</a>
                         </div>
-
                         <!-- Collect the nav links, forms, and other content for toggling -->
                         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                             <ul class="nav navbar-nav">
-
                                 <li class="dropdown">
                                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">KILLE</a>
                                     <ul class="dropdown-menu" role="menu">
-                                        <li><a href="#">Action</a></li>
-                                        <li><a href="#">Another action</a></li>
-                                        <li><a href="#">Something else here</a></li>
-                                        <li class="divider"></li>
-                                        <li><a href="#">Separated link</a></li>
-                                        <li class="divider"></li>
-                                        <li><a href="#">One more separated link</a></li>
+                                        <div class="row kt">
+                                            <div class="col-xs-4 kt">
+                                                <li><h3>Överdelar</h3></li>
+                                                <li><a href="#">T-shirt</a></li>
+                                                <li><a href="#">Tröjor</a></li>
+                                                <li><a href="#">Skjortor</a></li>
+                                                <li><a href="#">Jackor</a></li>
+                                            </div>
+                                            <div class="col-xs-4 kt">
+                                                <li><h3>Underdelar</h3></li>
+                                                <li><a href="#">Jeans</a></li>
+                                                <li><a href="#">Chinos</a></li>
+                                            </div>
+                                            <div class="col-xs-4 kt">
+                                                <li><h3>Accessoarer</h3></li>
+                                                <li><a href="#">Mösssor</a></li>
+                                                <li><a href="#">Kepsar</a></li>
+                                            </div>
+                                        </div>
                                     </ul>
                                 </li>
-
                                 <li class="dropdown">
                                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">TJEJ</a>
                                     <ul class="dropdown-menu" role="menu">
-                                        <li><a href="#">Action</a></li>
-                                        <li><a href="#">Another action</a></li>
-                                        <li><a href="#">Something else here</a></li>
-                                        <li class="divider"></li>
-                                        <li><a href="#">Separated link</a></li>
-                                        <li class="divider"></li>
-                                        <li><a href="#">One more separated link</a></li>
+                                        <div class="row kt">
+                                            <div class="col-xs-4 kt">
+                                                <li><h3>Överdelar</h3></li>
+                                                <li><a href="#">T-shirt</a></li>
+                                                <li><a href="#">Tröjor</a></li>
+                                                <li><a href="#">Skjortor</a></li>
+                                                <li><a href="#">Jackor</a></li>
+                                            </div>
+                                            <div class="col-xs-4 kt">
+                                                <li><h3>Underdelar</h3></li>
+                                                <li><a href="#">Jeans</a></li>
+                                                <li><a href="#">Chinos</a></li>
+                                            </div>
+                                            <div class="col-xs-4 kt">
+                                                <li><h3>Accessoarer</h3></li>
+                                                <li><a href="#">Mösssor</a></li>
+                                                <li><a href="#">Kepsar</a></li>
+                                            </div>
+                                        </div>
                                     </ul>
                                 </li>
-
                             </ul>
                             <form class="navbar-form navbar-left" role="search">
                                 <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
                                 <div class="form-group">
                                     <input type="text" class="form-control" placeholder="SÖK">
                                 </div>
-
                             </form>
                             <ul class="nav navbar-nav navbar-right">
-
                                 <li class="dropdown">
                                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">LOGGA IN</a>
                                     <ul class="dropdown-menu" role="menu">
-                                        <li><a href="#">Action</a></li>
-                                        <li><a href="#">Another action</a></li>
-                                        <li><a href="#">Something else here</a></li>
-                                        <li class="divider"></li>
-                                        <li><a href="#">Separated link</a></li>
+                                        <div class="row LR">
+                                            <div class="col-xs-6 LR">
+                                                <h3>Logga in</h3>
+                                                <form role="form">
+                                                    <div class="form-group">
+                                                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Lösenord">
+                                                    </div>
+                                                    <div class="checkbox">
+                                                        <button type="submit" class="btn btn-default">Logga in</button>
+                                                        <label>
+                                                            <input type="checkbox"> Kom ihåg mig
+                                                        </label>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                            <div class="col-xs-6 LR">
+                                                <h3>Registrera</h3>
+                                                <form role="form">
+                                                    <div class="form-group">
+                                                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Lösenord">
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Bekräfta Lösenord">
+                                                    </div>
+                                                    <button type="submit" class="btn btn-default">Regestrera dig</button>
+                                            </form> 
+                                        </div>
+                                        </div>
+                                    </ul>
+                                </li>
+                                <li><a href="#">KUNDTJÄNST</a></li>
+
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span></a>
+                                    <ul class="dropdown-menu" role="menu">
+                                        <div class="tomkundvagn">
+                                        <p>Kundvagnen är tom<p>
+                                        </div>                                       
                                     </ul>
                                 </li>
 
-                                <li><a href="#">KUNDTJÄNST</a></li>
-                                <li><a href="#"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span></a></li>
                             </ul>
                         </div><!-- /.navbar-collapse -->
                     </div><!-- /.container-fluid -->
